c8255d853168a13dc644803e49019926790e44d9
DESKTOP-5P9OAHR\user <test@test.com>
2024-08-13 17:18:41 +0900

    springLesson_240813

---
+++ src/main/java/com/memo/batch/TestJob.java
@@ -0,0 +1,17 @@
+package com.memo.batch;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class TestJob {
+	
+	@Scheduled(cron = "0 */1 * * * *")
+	public void task() {
+		log.info("####$$$$$ task batch 수행!!!!");
+	}
+	
+}
