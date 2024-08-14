d4624b475eec671421109b352b6539dd647f8dc4
DESKTOP-5P9OAHR\user <test@test.com>
2024-08-14 17:22:35 +0900

    springLesson_240814

--- src/main/java/com/memo/post/PostController.java
+++ src/main/java/com/memo/post/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.memo.aop.TimeTrace;
 import com.memo.post.bo.PostBO;
 import com.memo.post.domain.Post;
 
@@ -21,6 +22,7 @@ public class PostController {
 	@Autowired
 	private PostBO postBO;
 	
+	@TimeTrace
 	@GetMapping("/post-list-view")
 	public String postListView(
 			@RequestParam(value = "prevId", required = false) Integer prevIdParam,
